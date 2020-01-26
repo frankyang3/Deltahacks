dc02de4d3de83e68a029b079d920c54703139b67
frankyang3 <frankyang3@gmail.com>
2020-01-26 00:53:09 -0500

    buttons

--- deltahacks/src/Components/Signup.js
+++ deltahacks/src/Components/Signup.js
@@ -22,7 +22,7 @@ class Signup extends Component {
     }
     render() {
         return (
-            <div>
+            
                 <div className="form-container">
                     <h1>Just Chat</h1>
                     <form onSubmit={this.handleSubmit} className="form">
@@ -30,14 +30,15 @@ class Signup extends Component {
                         <input type="email" name="username" onChange={this.handleChange} className="input" />
                         <label htmlFor="email">What is your room ID?</label>
                         <input type="text" name="rmId" onChange={this.handleChange} className="input" />
+                        <input type="checkbox" name="rmId" onChange={this.handleChange} value = "0f82ea89-b85f-44cc-aac8-069e3c3c885f" /> Room 1
+                        <input type="checkbox" name="rmId" onChange={this.handleChange} value = "4ff19752-6fa4-4abe-a55c-a18c0f4cc5ca" /> Room 2 
+                        <input type="checkbox" name="rmId" onChange={this.handleChange} value = "9b93b260-861b-4f86-b2e0-fe4abc70e2bc" /> Room 3 
+                        <input type="checkbox" name="rmId" onChange={this.handleChange} value = "f228987c-dcdc-4e62-8de1-064c074a5cd8" /> Room 4 <br/> <br/>   
                         <button className="submit">Submit</button>
                     </form>
                 </div>
-                <div>
-                    <p> ROOM 1: 0f82ea89-b85f-44cc-aac8-069e3c3c885f  </p>
-                    <p> ROOM 2: 4ff19752-6fa4-4abe-a55c-a18c0f4cc5ca  </p>
-                </div>
-            </div>
+                
+           
         )
     }
 }
